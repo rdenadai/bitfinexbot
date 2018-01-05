f7caff26c8e94355d3a7700ea47b4188ea602e92
Rodolfo De Nadai <rdenadai@ccuec.unicamp.br>
2018-01-05 09:45:14 -0200

    Some adjustments... and create a start script.

--- classes/logger.py
+++ classes/logger.py
@@ -11,11 +11,11 @@ class Logger:
 
     @staticmethod
     def remove_logger_file():
-        with open('log.txt', 'w') as fhandler:
+        with open('logs/bot.log', 'w') as fhandler:
             fhandler.write('')
 
     @staticmethod
     def save_logger_file(_data):
         timestamp = datetime.datetime.fromtimestamp(time.time())
-        with open('log.txt', 'a') as fhandler:
+        with open('logs/bot.log', 'a') as fhandler:
             fhandler.write(f'{_data} : {timestamp.strftime("%d-%m-%Y %H:%M:%S")}\n')
